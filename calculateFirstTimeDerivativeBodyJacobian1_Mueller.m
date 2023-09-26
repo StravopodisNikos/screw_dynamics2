b56ade0af6e5dc4871f9e78d1ca7939d93cf48b9
SyrosRoboticsResearch1 <stravnik.rob@gmail.com>
2023-09-26 17:40:44 +0300

    this is a mess but works for main_idosc.m

--- calculateFirstTimeDerivativeBodyJacobian1_Mueller.m
+++ calculateFirstTimeDerivativeBodyJacobian1_Mueller.m
@@ -1,4 +1,4 @@
-function [dJb_ij_dt] = calculateFirstTimeDerivativeBodyJacobian1_Mueller(Jb,dq)
+function [dJb_ij_dt] = calculateFirstTimeDerivativeBodyJacobian1_Mueller(Jb,dq,USE_SYM)
 % Implements the FIRST (=) of eq.(8) in Mueller Dynamics
 % Jb_ij, where i denotes the body of the manipulator the Body Jacobian
 % relates to and j the current joint variable counter
@@ -6,14 +6,21 @@ function [dJb_ij_dt] = calculateFirstTimeDerivativeBodyJacobian1_Mueller(Jb,dq)
 nDoF = size(Jb,2);
 i = nDoF; % assumes the Jacobian relates to the {T} frame after the last active twist
 
-dJb_ij = sym(zeros(6,nDoF),'d');
+if (USE_SYM)
+    dJb_ij = sym(zeros(6,nDoF),'d');
+else
+    dJb_ij = zeros(6,nDoF);
+end
 
 for j=1:i
-    dJ = sym(zeros(6,1),'d');
-    for k=j+1:i
-        dJ = dJ + liebracket_426_new(Jb(:,j), Jb(:,k)) * dq(k);
+    if (USE_SYM)
+        dJ = sym(zeros(6,1),'d');
+    else
+        dJ = zeros(6,1);
     end
-    
+    for k=j+1:i
+        dJ = dJ + liebracket_426_new(Jb(:,j,4), Jb(:,k,4)) * dq(k); % For the Jacobian that relates to the {T} frame
+    end
     dJb_ij(:,j) = dJ;
 end
 
